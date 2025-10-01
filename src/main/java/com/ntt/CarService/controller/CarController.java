b33e199a3b017437fb8c0035436d2c5715bbe50d
Leti-NTT <letizia.cravotta@emeal.nttdata.com>
2025-10-01 16:03:04 +0200

    Refactor car endpoints to use appropriate HTTP status codes for responses

--- src/main/java/com/ntt/CarService/controller/CarController.java
+++ src/main/java/com/ntt/CarService/controller/CarController.java
@@ -31,21 +31,21 @@ public class CarController {
     @PostMapping("/car")
     public ResponseEntity<?> createCar(@RequestBody Car car) {
         carService.createCar(car);
-        return ResponseEntity.ok("Car created successfully");
+        return ResponseEntity.status(201).body("Car created successfully");
     }
 
     @PutMapping("/car/{id}")
     public ResponseEntity<String> updateCar(@PathVariable Long id, @RequestBody Car updatedCar) {
         if (carService.updateCar(id, updatedCar))
             return ResponseEntity.ok("Car updated successfully");
-        return ResponseEntity.ok("Car with ID " + id + " not found");
+        return ResponseEntity.status(404).body("Car with ID " + id + " not found");
     }
 
     @DeleteMapping("/car/{id}")
     public ResponseEntity<String> deleteCar(@PathVariable Long id) {
         if (carService.deleteCar(id))
             return ResponseEntity.ok("Car deleted successfully");
-        return ResponseEntity.ok("Car with ID " + id + " not found");
+        return ResponseEntity.status(404).body("Car with ID " + id + " not found");
     }
 
 }
